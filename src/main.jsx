915fc847987211af240a609c7f938d275232d0a4
cycling-lion-cub <omglion2club@gmail.com>
2026-09-10 16:45:33 +0900

    fix: 글자 크기를 의도치 않게 줄이는 문제 수정

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,19 @@ import { createRoot } from 'react-dom/client';
 
 import '@/styles/reset.css';
 
-import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+// import App from './App.jsx';
+import { DetailPage } from './pages/detail/DetailPage.jsx';
+{
+  /* DetailPage 페이지 렌더링을 위한 임시 추가 */
+}
+import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {/* DetailPage 페이지 렌더링을 위한 임시 주석처리 */}
+    {/* <App /> */}
     <BrowserRouter>
-      <App />
+      <DetailPage />
     </BrowserRouter>
   </StrictMode>,
 );
