0ffe74c4f8d0b10fa2b935828446629ee5ce0688
cycling-lion-cub <omglion2club@gmail.com>
2026-09-11 09:12:48 +0900

    chore: 라우팅 통합 작업을 위해 단독 테스트용 임시 코드 원상복구

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
-import '@/styles/reset.css';
-
-// import App from './App.jsx';
-import { DetailPage } from './pages/detail/DetailPage.jsx';
-{
-  /* DetailPage 페이지 렌더링을 위한 임시 추가 */
-}
 import { BrowserRouter } from 'react-router';
+import '@/styles/reset.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* DetailPage 페이지 렌더링을 위한 임시 주석처리 */}
-    {/* <App /> */}
     <BrowserRouter>
-      <DetailPage />
+      <App />
     </BrowserRouter>
   </StrictMode>,
 );
--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,1 +1,1 @@
-export { DetailPage } from './DetailPage';
+export { default as DetailPage } from './DetailPage.jsx';
