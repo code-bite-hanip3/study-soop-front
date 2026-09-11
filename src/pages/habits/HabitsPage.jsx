23b967b9c9f1e71cbfef352d6e92648ac46528ce
Limug-g <dmlrbs94@gmail.com>
2026-09-11 15:14:55 +0900

    fix: HabitPage.jsx에서 Layout태그 삭제

--- src/pages/habits/HabitsPage.jsx
+++ src/pages/habits/HabitsPage.jsx
@@ -3,7 +3,6 @@ import { NavButton } from '@/components/Button/NavButton';
 import { Frame } from '@/components/Frame';
 import { HabitList } from '@/components/HabitList/HabitList';
 import { HabitOpenModal } from '@/components/HabitOpenModal';
-import { Layout } from '@/components/Layout';
 import { useState } from 'react';
 import { useParams } from 'react-router';
 
@@ -16,48 +15,46 @@ export function HabitsPage() {
 
   return (
     <>
-      <Layout>
-        <Frame>
-          <div className={styles.head}>
-            <div className={styles.habitNav}>
-              <h2>스터디 이름</h2>
-              <div className={styles.habitBtns}>
-                <NavButton to={`/studies/${studyId}/focus`} size="type01">
-                  오늘의 집중
-                </NavButton>
-                <NavButton to={'/'} size="type02">
-                  홈
-                </NavButton>
-              </div>
-            </div>
-            <div className={styles.habitDate}>
-              <p>현재시간</p>
-              <div className={styles.date}>{nowDate}</div>
+      <Frame>
+        <div className={styles.head}>
+          <div className={styles.habitNav}>
+            <h2>스터디 이름</h2>
+            <div className={styles.habitBtns}>
+              <NavButton to={`/studies/${studyId}/focus`} size="type01">
+                오늘의 집중
+              </NavButton>
+              <NavButton to={'/'} size="type02">
+                홈
+              </NavButton>
             </div>
           </div>
-          <div className={styles.habitFrame}>
-            <div className={styles.habitBox}>
-              <div className={styles.todayHabit}>
-                <div className={styles.spacer}>여백균형</div>
-                <p>오늘의 습관</p>
-                <button
-                  className={styles.listEdit}
-                  onClick={() => setIsModalOpen(true)}
-                >
-                  목록수정
-                </button>
-              </div>
-              <ul className={styles.habitList}>
-                <HabitList />
-              </ul>
-            </div>
+          <div className={styles.habitDate}>
+            <p>현재시간</p>
+            <div className={styles.date}>{nowDate}</div>
           </div>
-          {/* 조건문으로 모달창이 열리고 닫히는걸 구현 */}
-          {isModalOpen && (
-            <HabitOpenModal onClose={() => setIsModalOpen(false)} />
-          )}
-        </Frame>
-      </Layout>
+        </div>
+        <div className={styles.habitFrame}>
+          <div className={styles.habitBox}>
+            <div className={styles.todayHabit}>
+              <div className={styles.spacer}>여백균형</div>
+              <p>오늘의 습관</p>
+              <button
+                className={styles.listEdit}
+                onClick={() => setIsModalOpen(true)}
+              >
+                목록수정
+              </button>
+            </div>
+            <ul className={styles.habitList}>
+              <HabitList />
+            </ul>
+          </div>
+        </div>
+        {/* 조건문으로 모달창이 열리고 닫히는걸 구현 */}
+        {isModalOpen && (
+          <HabitOpenModal onClose={() => setIsModalOpen(false)} />
+        )}
+      </Frame>
     </>
   );
 }
