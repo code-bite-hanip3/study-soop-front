4db8da7cc89125934968950038b4f5ba60907bac
ha kyeong <hess93hess93@gmail.com>
2026-09-12 23:19:40 +0900

    refactor: 리액트 쿼리로 실시간 연동 간소화

--- src/components/TotalPointChip/TotalPointChip.jsx
+++ src/components/TotalPointChip/TotalPointChip.jsx
@@ -1,43 +1,20 @@
 import styles from './TotalPointChip.module.css';
-import { useEffect, useRef, useState } from 'react';
 import { timer } from '@/api/focusSessions';
 import point_chip_imoji from '@/assets/point_chip_imoji.svg';
-import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router';
+import { useQuery } from '@tanstack/react-query';
 
 export function TotalPointChip() {
-  const [totalPoint, setTotalPoint] = useState(0);
   const { studyId } = useParams();
-  const intervalRef = useRef(null);
 
-  useEffect(() => {
-    if (!studyId) return;
+  const { data } = useQuery({
+    queryKey: ['totalPoint', studyId],
+    queryFn: () => timer.getTotalCount(studyId),
+    enabled: !studyId,
+    refetchInterval: 20000,
+  });
 
-    let isMounted = true;
-
-    const fetching = async () => {
-      try {
-        const { earnedPoint } = await timer.getTotalCount(studyId);
-
-        if (isMounted) {
-          setTotalPoint(earnedPoint);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    // 처음 마운트 되었을 때 - 없으면 20초 후 받아옴
-    fetching();
-
-    // 20초마다 반복
-    intervalRef.current = setInterval(fetching(), 20000);
-
-    return () => {
-      isMounted = false;
-      clearInterval(intervalRef.current);
-      intervalRef.current = null;
-    };
-  }, [studyId]);
+  const totalPoint = data?.earnedPoint ?? 0;
 
   return (
     <div className={styles.chip}>
@@ -46,7 +23,7 @@ export function TotalPointChip() {
         alt="포인트 칩 이모지"
         className={styles.chipImoji}
       />
-      <span>{totalPoint ?? 0}P 획득</span>
+      <span>{totalPoint}P 획득</span>
     </div>
   );
 }
