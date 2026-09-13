0de4d15fa402f32f817c23fa1405733b7a25d833
ha kyeong <hess93hess93@gmail.com>
2026-09-13 15:20:17 +0900

    refactor: 기록 목록 중복 방지로 useRef 삭제 및 클로저 기반 ignore로 변경

--- src/pages/focus/RecordList/RecordList.jsx
+++ src/pages/focus/RecordList/RecordList.jsx
@@ -1,5 +1,5 @@
 import { timer } from '@/api/focusSessions';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './RecordList.module.css';
 import dayjs from 'dayjs';
 import { formatToRelativeTime } from '@/utils/koreaServerTime.js';
@@ -8,7 +8,6 @@ export function RecordList() {
   const [recordList, setRecordList] = useState([]);
   const [cursorId, setCursorId] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-  const fetchingOnce = useRef(false); // 배포 이전에 삭제
   // const { studyId } = useParams();
   const studyId = '126d30dc-bf24-4a65-be40-951fb9d1d205';
 
@@ -30,10 +29,31 @@ export function RecordList() {
 
   // 페이지 처음
   useEffect(() => {
-    if (fetchingOnce.current) return;
-    fetchingOnce.current = true;
+    let ignore = false;
+    const getRecordList = async (cursorId = '') => {
+      try {
+        setIsLoading(true);
+        const { nextCursor, recordList } = await timer.getRecordList(
+          studyId,
+          cursorId,
+        );
+
+        if (ignore) return;
+
+        setCursorId(nextCursor ?? '');
+        setRecordList((prev) => [...prev, ...recordList]);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
     getRecordList();
-  }, []);
+    return () => {
+      ignore = true;
+    };
+  }, [studyId]);
 
   return (
     <>
