83b77027120990a6ddd263444757a1309f482d29
Limug-g <dmlrbs94@gmail.com>
2026-09-16 13:53:41 +0900

    feat: 모달 커스텀 클래스이름 추가 할 수 있도록 Modal.jsx에 props추가

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import styles from './Modal.module.css';
 
-
 //**사용법
 // 사용하는 곳에서 <Modal> 넣어야 되는 내용들 </Modal> 이렇게 작성하면 됩니다. {children} props로 들어감
 // onClose는 모달창을 닫기 위함. 모달창 뒷 배경 클릭시 닫힘 기능(기본)
@@ -9,9 +8,11 @@ import styles from './Modal.module.css';
 // onClose 를 사용하기 위해 각자 작업 페이지에 자기만의 state를 만들어야됨
 // 예시)
 // 각자 페이지 : const [isModalOpen, setIsModalOpen] = useState(false); 가 있어야됨 변수 이름은 자유
-// 모달이 열리는 페이지 : onClose={() => setIsModalOpen(false)} 이게 props로 들어가야됨 
-export function Modal({children, onClose}){
-    useEffect(() => {
+// 모달 사용하는 곳 : onClose={() => setIsModalOpen(false)} 이게 props로 들어가야됨
+//                className = {styles.클래스이름}으로 props 사용
+//                스타일 적용 순서 떄문에 모달 사용하는 파일의 css 에서 커스텀 클래스 이름 앞에 div를 붙여야됨
+export function Modal({ children, onClose, className }) {
+  useEffect(() => {
     // 모달이 열리는 순간(컴포넌트가 마운트되는 순간) 실행
     document.body.style.overflow = 'hidden';
 
@@ -20,11 +21,14 @@ export function Modal({children, onClose}){
       document.body.style.overflow = '';
     };
   }, []); // 빈 배열 -> 마운트/언마운트 시에만 실행
-  
-  return(
+
+  return (
     <>
       <div className={styles.wrapModal} onClick={onClose}>
-        <div className={styles.mainModal} onClick={(e) => e.stopPropagation()}>
+        <div
+          className={`${styles.mainModal} ${className ?? ''}`}
+          onClick={(e) => e.stopPropagation()}
+        >
           {children}
         </div>
       </div>
